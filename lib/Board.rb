390fc64742eded3a5546a8d085996a96704fe77b
Paulo Carrano <paulo.carrano@beepsaude.com.br>
2025-09-24 10:36:39 -0300

    Adicionando metodo para preencher o tabuleiro

--- lib/Board.rb
+++ lib/Board.rb
@@ -3,15 +3,36 @@ class Board
 
     def initialize
         @board = Array.new(8) {Array.new(8)}
+        populate_board
     end
 
-    # Parece meio doido mas da pra gente chamar board[pos] pra ter o valor da casa
+    def populate_board
+        (0..7).each { |col| self[[1, col]] = Pawn.new(:white, self, [1, col]) }
+        self[[0, 0]] = Rook.new(:white, self, [0, 0])
+        self[[0, 1]] = Knight.new(:white, self, [0, 1])
+        self[[0, 2]] = Bishop.new(:white, self, [0, 2])
+        self[[0, 3]] = Queen.new(:white, self, [0, 3])
+        self[[0, 4]] = King.new(:white, self, [0, 4])
+        self[[0, 5]] = Bishop.new(:white, self, [0, 5])
+        self[[0, 6]] = Knight.new(:white, self, [0, 6])
+        self[[0, 7]] = Rook.new(:white, self, [0, 7])
+
+        (0..7).each { |col| self[[6, col]] = Pawn.new(:black, self, [6, col]) }
+        self[[7, 0]] = Rook.new(:black, self, [7, 0])
+        self[[7, 1]] = Knight.new(:black, self, [7, 1])
+        self[[7, 2]] = Bishop.new(:black, self, [7, 2])
+        self[[7, 3]] = Queen.new(:black, self, [7, 3])
+        self[[7, 4]] = King.new(:black, self, [7, 4])
+        self[[7, 5]] = Bishop.new(:black, self, [7, 5])
+        self[[7, 6]] = Knight.new(:black, self, [7, 6])
+        self[[7, 7]] = Rook.new(:black, self, [7, 7])
+    end
+
     def [](pos) # Get do acesso às casas com as coordenadas [linha, coluna]
         row, col = pos
         @board[row][col]
     end
 
-    # Parece meio doido também mas da pra gente chamar board[pos] = value pra alterar o valor da casa
     def []=(pos, value) # Set da modificação das casas com as coordenadas [linha, coluna]
         row, col = pos
         @board[row][col] = value
